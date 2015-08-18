6d21e3cce35414ad300a8db18d31ac16d19925ac
Roni Maimon <ronimaimon@gmail.com>
2015-08-19 00:59:14 +0300

    try to run single sl on local computer, something is off

--- single_subject_sl.py
+++ single_subject_sl.py
@@ -2,27 +2,19 @@
 
 from mvpa2.suite import *
 from mvpa2.cmdline.helpers import arg2ds
-filename = sys.argv[1]
-radius   = sys.argv[2]
-		
-output_basename = os.path.join('{}_r{}_c-{}'.format(filename, radius,'linear'))
-print output_basename
-glm_dataset = arg2ds(filename)
-do_searchlight(glm_dataset, radius, output_basename,
-					with_null_prob)
 
 def do_searchlight(glm_dataset, radius, output_basename, with_null_prob=False):
 	clf  = LinearCSVMC(space='condition')
 #		clf = RbfCSVMC(C=5.0)
-	splt = NFoldPartitioner(attr='chunks')
+	splt = NFoldPartitioner()
 	cv = CrossValidation(clf,splt, 
 			     errorfx=mean_match_accuracy,
 			     enable_ca=['stats'], postproc=mean_sample() )
 	distr_est = []
 	if with_null_prob:
-		"""
-		also test:
-		permutator = AttributePermutator('targets', count=100,
+		
+		
+		permutator = AttributePermutator('condition', count=100,
 				  limit='chunks')
 		distr_est = MCNullDist(permutator, tail='left',
 			enable_ca=['dist_samples'])
@@ -35,20 +27,23 @@ def do_searchlight(glm_dataset, radius, output_basename, with_null_prob=False):
 					     enable_ca=['roi_sizes'])
 		distr_est = MCNullDist(repeater,tail='left', measure=null_sl,
 				       enable_ca=['dist_samples'])
+		"""
 		sl = sphere_searchlight(cv, radius=radius, space='voxel_indices',
 				null_dist=distr_est,
-				enable_ca=['roi_sizes'])
+				enable_ca=['roi_sizes','roi_feature_ids'])
 	else:
 
-		sl = sphere_searchlight(cv, radius=radius, space='voxel_indices',
-				enable_ca=['roi_sizes'])
-	ds = glm_dataset.copy(deep=False,
-			       sa=['condition','chunks'],
-			       fa=['voxel_indices'],
-			       a=['mapper'])
+		sl = sphere_searchlight(cv, nproc=20,radius=radius, space='voxel_indices',
+				enable_ca=['roi_sizes','roi_feature_ids'])
+	#ds = glm_dataset.copy(deep=False,
+	#		       sa=['condition','chunks'],
+	#		       fa=['voxel_indices'],
+	#		       a=['mapper'])
 	now = datetime.datetime.now()
+	print glm_dataset.shape
 	print now
-	sl_map = sl(ds)
+	debug.active += ["SLC"]
+	sl_map = sl(glm_dataset)
 	print datetime.datetime.now() - now
 	errresults = map2nifti(sl_map,
 				 imghdr=glm_dataset.a.imghdr)
@@ -70,4 +65,12 @@ def do_searchlight(glm_dataset, radius, output_basename, with_null_prob=False):
 				 imghdr=glm_dataset.a.imghdr)
 		nullprob_results.to_filename('{}-cdf.nii.gz'.format(output_basename))
 		
-
+if __name__ == '__main__':
+	filename = sys.argv[1]
+	radius   = sys.argv[2]
+	print filename		
+	output_basename = os.path.join('{}_r{}_c-{}'.format(filename, radius,'linear'))
+	print output_basename
+	glm_dataset = arg2ds([filename])
+	do_searchlight(glm_dataset, radius, output_basename,
+						False)
