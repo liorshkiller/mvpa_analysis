55e83459f01daf993d08e4c540a807155d2da96b
Roni Maimon <ronimaimon@gmail.com>
2015-08-23 16:43:18 +0300

    single subject sl now working

--- single_subject_sl.py
+++ single_subject_sl.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python
 
-from mvpa2.suite import *
+from mvpa2.measures.searchlight import sphere_searchlight
+from mvpa2.clfs.svm import LinearCSVMC
+from mvpa2.generators.partition import NFoldPartitioner
+from mvpa2.measures.base import CrossValidation
+from mvpa2.misc.errorfx import mean_match_accuracy
+from mvpa2.mappers.fx import mean_sample
 from mvpa2.cmdline.helpers import arg2ds
-
+from mvpa2.generators.permutation import AttributePermutator
+from mvpa2.clfs.stats import MCNullDist
+from mvpa2.datasets.mri import map2nifti
+import sys
+import os.path
 def do_searchlight(glm_dataset, radius, output_basename, with_null_prob=False):
 	clf  = LinearCSVMC(space='condition')
 #		clf = RbfCSVMC(C=5.0)
@@ -12,8 +21,6 @@ def do_searchlight(glm_dataset, radius, output_basename, with_null_prob=False):
 			     enable_ca=['stats'], postproc=mean_sample() )
 	distr_est = []
 	if with_null_prob:
-		
-		
 		permutator = AttributePermutator('condition', count=100,
 				  limit='chunks')
 		distr_est = MCNullDist(permutator, tail='left',
@@ -33,18 +40,14 @@ def do_searchlight(glm_dataset, radius, output_basename, with_null_prob=False):
 				enable_ca=['roi_sizes','roi_feature_ids'])
 	else:
 
-		sl = sphere_searchlight(cv, nproc=20,radius=radius, space='voxel_indices',
+		sl = sphere_searchlight(cv, radius=radius, space='voxel_indices',
 				enable_ca=['roi_sizes','roi_feature_ids'])
 	#ds = glm_dataset.copy(deep=False,
 	#		       sa=['condition','chunks'],
 	#		       fa=['voxel_indices'],
 	#		       a=['mapper'])
-	now = datetime.datetime.now()
-	print glm_dataset.shape
-	print now
-	debug.active += ["SLC"]
+	#debug.active += ["SLC"]
 	sl_map = sl(glm_dataset)
-	print datetime.datetime.now() - now
 	errresults = map2nifti(sl_map,
 				 imghdr=glm_dataset.a.imghdr)
 	errresults.to_filename('{}-acc.nii.gz'.format(output_basename))
@@ -67,10 +70,11 @@ def do_searchlight(glm_dataset, radius, output_basename, with_null_prob=False):
 		
 if __name__ == '__main__':
 	filename = sys.argv[1]
-	radius   = sys.argv[2]
+	radius   = int(sys.argv[2])
 	print filename		
 	output_basename = os.path.join('{}_r{}_c-{}'.format(filename, radius,'linear'))
 	print output_basename
-	glm_dataset = arg2ds([filename])
-	do_searchlight(glm_dataset, radius, output_basename,
-						False)
+	ds = arg2ds([filename])
+	do_searchlight(ds, radius, output_basename ,
+						#False)
+						True)
